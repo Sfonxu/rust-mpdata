8b8749b6d9fa5f762638205d42e1bbff17d2a852
sfonxu <wronski.michal4@wp.pl>
2025-04-29 18:01:26 +0200

    refactor functions

--- src/field.rs
+++ src/field.rs
@@ -1,5 +1,5 @@
 use std::vec::Vec;
-use ndarray::{Array};
+use ndarray::{Array, Zip, s};
 #[allow(non_snake_case)]
 pub fn F(left: f64, right: f64, c: f64) -> f64 {
 	return 0.5*(c+c.abs())*left + 0.5*(c-c.abs())*right;
@@ -9,8 +9,9 @@ type Field<D> = Array<f64,D>;
 
 pub struct Solver<D> {
     pub field: Field<D>,
-    pub courant_number: Field<D>,
-    pub c_err: Field<D>,
+    courant_number: Field<D>,
+    c_err: Field<D>,
+    field_store: Field<D>,
     steps_done: usize,
 }
 
@@ -26,33 +27,30 @@ pub type Solver3D = Solver<Ix3>;
 
 impl Solver1D {
     fn get_c_err (&mut self) {
-	let mut c = self.courant_number.clone();
-	let len = self.field.len();
-	for (x, val) in c.indexed_iter_mut() {
-	    if x > 0 && x < len-1 {
-		*val = ((*val).abs()-(*val)*(*val)) * (self.field[x]-self.field[x-1])/(self.field[x]+self.field[x-1]);
-	    }
-	}
-	self.c_err = c;
+	Zip::from(self.c_err.slice_mut(s![1..]))
+	    .and(self.field.windows(2))
+	    .for_each(|c, w| {
+		*c = ((*c).abs()-(*c)*(*c))*(w[0]-w[1])/(w[0]+w[1]);
+	    });	
     }
+
     
     fn upwind(&mut self, mpdata: bool)  {
-	let s = &self.field.clone();
-	let len = self.field.len();
 	let c: &Field1D;
+	self.field_store = self.field.clone();
 	if !mpdata {
 	    c = &self.courant_number;
 	} else {
+	    self.get_c_err();
 	    c = &self.c_err;
 	}
-	for (x, psi) in self.field.indexed_iter_mut() {
-	    if x > 0 && x < len-1 {
-		*psi = *psi - (
-		    F(s[x], s[x+1], c[x]) -
-		    F(s[x-1], s[x], c[x-1])
-		);
-	    }
-	}
+	Zip::from(self.field_store.slice_mut(s![1..-1]))
+	    .and(self.field.windows(3))
+	    .and(c.slice(s![1..]).windows(2))
+	    .for_each(|x, w, c|{
+		*x = *x - (F(w[1], w[2], c[1]) - F(w[0], w[1], c[0]));
+	    });
+	self.field = self.field_store.clone();
 	self.steps_done += 1;
     }
 
@@ -66,7 +64,6 @@ impl Solver1D {
 	    "MPDATA" => {
 		for _ in 0..time_steps-1 {
 		    self.upwind(false);
-		    self.get_c_err();
 		    self.upwind(true);
 		}
 	    }
@@ -78,11 +75,12 @@ impl Solver1D {
 	    _ => { panic!("No method chosen! Availible methods: \"MPDATA\" and \"upwind\".") }
 	}
     }    
-    pub fn assemble(vel: f64, dt: f64, dx: f64, len: usize, linstart: f64, linend: f64, nsteps: usize) -> Self {
+    pub fn assemble(vel: f64, dt: f64, dx: f64, linstart: f64, linend: f64, nsteps: usize) -> Self {
 	Solver1D {
 	    field: Field1D::linspace(linstart, linend, nsteps),
-	    courant_number: (vel*dt)/dx*Field1D::ones(len),
-	    c_err: (vel*dt)/dx*Field1D::ones(len),
+	    field_store: Field1D::linspace(linstart, linend, nsteps),
+	    courant_number: (vel*dt)/dx*Field1D::ones(nsteps),
+	    c_err: (vel*dt)/dx*Field1D::ones(nsteps),
 	    steps_done: 0
 	}
     } 
